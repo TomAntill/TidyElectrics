90f8f48e16aed8514988288e3f317eda754b860f
Thomas Antill <thomasantill92@gmail.com>
2023-12-13 20:11:29 +0000

    changes to settings

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -3,16 +3,14 @@ const WEBSITE_SETTINGS = {
   WEBSITE_ID: "TidyElectrics",
   UNSET_WEBSITE_ID: "Unset",
   BACKEND_SERVICES_BASE_PATH:
-  "https://localhost:44311/",
-    //"https://tomantillwebdevservices.azurewebsites.net/",
+    "https://tomantillwebdevservices.azurewebsites.net/",
   BACKEND_SERVICES_JS_PATH: "assets/js/websites/",
 };
 
 function postLoadScript(file, isModule, appendToHead, callback) {
   const scriptElement = document.createElement("script");
-  scriptElement.src = `${WEBSITE_SETTINGS.BACKEND_SERVICES_BASE_PATH}${
-    WEBSITE_SETTINGS.BACKEND_SERVICES_JS_PATH
-  }${isModule ? `services/modules/` : ``}${file}`;
+  scriptElement.src = `${WEBSITE_SETTINGS.BACKEND_SERVICES_BASE_PATH}${WEBSITE_SETTINGS.BACKEND_SERVICES_JS_PATH
+    }${isModule ? `services/modules/` : ``}${file}`;
   if (isModule) {
     scriptElement.type = "module";
   }
